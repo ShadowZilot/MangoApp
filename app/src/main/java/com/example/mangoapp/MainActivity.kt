3bb84b90841e233e80181df771cb102b339ac944
ShadowZilot <e.ponomaryow2017@yandex.ru>
2023-05-17 09:09:46 +0300

    Added navigation into app

--- app/src/main/java/com/example/mangoapp/MainActivity.kt
+++ app/src/main/java/com/example/mangoapp/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.mangoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
